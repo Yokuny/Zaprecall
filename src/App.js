aed290ed87d667bde68dd3761d368029f0ff9439
yokuny <felipe.vni@hotmail.com>
2023-02-28 18:44:13 -0300

    first Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import styled from "styled-components";
+const AppStyled = styled.div`
+  background-color: #f5f5;
+`;
 const App = () => {
   return (
-    <div>
+    <AppStyled>
       <h1>Hello World!</h1>
-    </div>
+    </AppStyled>
   );
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import reactDOM from "react-dom";
 import App from "./App";
+import ResetCss from "./components/ResetCss";
+import Layout from "./components/Layout";
 const root = document.getElementById("root");
-reactDOM.render(<App />, root);
+reactDOM.render(
+  <>
+    <ResetCss />
+    <Layout>
+      <App />
+    </Layout>
+  </>,
+  root
+);
